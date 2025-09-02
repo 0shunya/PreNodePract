dd4425637e641105929b8e999d2545a238c76568
0shunya <shloksingh2805@gmail.com>
2025-09-03 01:17:36 +0530

    Caps

---
+++ intermediate/Caps.js
@@ -0,0 +1,18 @@
+function capitalize(str){
+    let one = str.split(" ").slice();
+    console.log(one);
+    let result = []
+    for (let i = 0; i < one.length; i++) {
+        const element = one[i];
+        // console.log(element);
+        
+        let capzz = element[0].toUpperCase() + element.slice(1);
+        console.log(capzz);
+        
+        result.push(capzz)
+    }
+   return  console.log(result.join(" "));
+       
+}
+
+capitalize("i love coding");
